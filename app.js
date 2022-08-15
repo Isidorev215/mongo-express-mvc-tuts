9f84ab67fda295513cf04fe7df6ab4554ff32cd4
Isidorev215 <nwaiwuisidore@gmail.com>
2022-08-15 17:14:18 +0100

    Revert to netninja style

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 const express = require('express');
-const { connectToDb } = require('./config/db');
-
+const { connectToDb, getDb } = require('./db')
 
 // init app and middleware
 
 const app = express();
 
 // db connection
+let db;
 connectToDb((err) => {
   if(!err){
     app.listen(3000, () => {
       console.log('app Listening on port 3000')
     })
   }
+  db = getDb();
 })
 
 
 // routes
-app.use('/books', require('./routes/booksRoute'))
+app.get('/books', (req, res) => {
+  let books = [];
+  db.collection('books').find().sort({ author: 1 }).forEach(book => {
+    books.push(book)
+  })
+  .then(() => res.status(200).json(books))
+  .catch(() => res.status(500).json({error: 'Could Not fetch documents'}))
 
-
+})
