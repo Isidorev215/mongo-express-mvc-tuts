c3b6a88576747dba72b7480cbf4c827b3a09de15
Isidorev215 <nwaiwuisidore@gmail.com>
2022-08-16 12:02:56 +0100

    added more routes

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const { ObjectId } = require('mongodb')
 const express = require('express');
 const { connectToDb, getDb } = require('./db')
 
-// init app and middleware
-
+// init app and top-level middlewares
 const app = express();
+app.use(express.json());
 
 // db connection
 let db;
@@ -43,4 +43,40 @@ app.get('/books/:id', (req, res) => {
     res.status(500).json({error: 'Not valid document'})
   }
 
+})
+
+app.post('/books', (req, res) => {
+  const book = req.body;
+  db.collection('books').insertOne(book)
+  .then(result => res.status(201).json(result))
+  .catch(err => res.status(500).json({error: 'Could not create a new document'}))
+})
+
+app.delete('/books/:id', (req, res) => {
+  let id = req.params.id;
+  if(ObjectId.isValid(id)){
+    db.collection('books').deleteOne({_id: ObjectId(id)})
+    .then(result => {
+      res.status(200).json(result)
+    })
+    .catch(err => res.status(500).json({error: 'Could not delete document'}))
+  }
+  else{
+    res.status(500).json({error: 'Not valid document'})
+  }
+})
+
+app.patch('/books/:id', (req, res) => {
+  let updates = req.body;
+  let id = req.params.id;
+  if(ObjectId.isValid(id)){
+    db.collection('books').updateOne({_id: ObjectId(id)}, {$set: updates})
+    .then(result => {
+      res.status(200).json(result)
+    })
+    .catch(err => res.status(500).json({error: 'Could not make updates to document'}))
+  }
+  else{
+    res.status(500).json({error: 'Not valid document'})
+  }
 })
