8ad822d77ecdcb2335361b5f08d2ddc3ce1784ec
Isidorev215 <nwaiwuisidore@gmail.com>
2022-08-16 10:07:57 +0100

    added books/:id route

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { ObjectId } = require('mongodb')
 const express = require('express');
 const { connectToDb, getDb } = require('./db')
 
@@ -25,5 +26,21 @@ app.get('/books', (req, res) => {
   })
   .then(() => res.status(200).json(books))
   .catch(() => res.status(500).json({error: 'Could Not fetch documents'}))
+
+})
+
+app.get('/books/:id', (req, res) => {
+  let id = req.params.id;
+  if(ObjectId.isValid(id)){
+    db.collection('books').findOne({_id: ObjectId(id)})
+    .then(doc => {
+      if(!doc) throw 'Book doesnt exist';
+      res.status(200).json(doc)
+    })
+    .catch(() => res.status(500).json({error: 'Could not fetch document'}))
+  }
+  else{
+    res.status(500).json({error: 'Not valid document'})
+  }
 
 })
